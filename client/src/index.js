f315808a70a00866909d998dedcd1e5fdb1d4712
Matthew Ratzke <matt@degree9.io>
2023-10-06 13:44:14 -0600

    update registry components

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,10 @@ import GlobalStyles from "./styles";
 import Pages from "./pages";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 import { ComponentRegistryProvider } from "./components/registry.js";
+import TrackTitle from "./components/track-title.js";
+import TrackInfo from "./components/track-info.js";
+import TrackModules from "./components/track-modules.js";
+import TrackReviews from "./components/track-reviews.js";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000",
@@ -11,6 +15,10 @@ const client = new ApolloClient({
 });
 
 const initialRegistry = {
+  'TrackTitle': TrackTitle,
+  'TrackInfo': TrackInfo,
+  'TrackModules': TrackModules,
+  'TrackReviews': TrackReviews,
 };
 
 ReactDOM.render(
